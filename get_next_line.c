7388d5ed5d34a505e0adb1b841eb5c6e75e3b144
ssolpang <iless3377@naver.com>
2022-03-11 00:02:19 +0900

    draft

--- get_next_line.c
+++ get_next_line.c
@@ -6,39 +6,85 @@
 /*   By: jkwak <jkwak@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/16 22:52:07 by jkwak             #+#    #+#             */
-/*   Updated: 2022/03/09 19:21:28 by jkwak            ###   ########.fr       */
+/*   Updated: 2022/03/10 23:57:52 by jkwak            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*remain(int fd)
+char	*read_line(int fd)
 {
 	char	*buf;
+	int		i;
 
 	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (buf == 0)
+	if (!buf)
 		return (NULL);
-	read(fd, buf, BUFFER_SIZE);
+	i = read(fd, buf, BUFFER_SIZE);
+	if (i == 0)
+		return (NULL);
 	buf[BUFFER_SIZE] = 0;
+	return (buf);
+}
+
+char	*edit_line(char *line, int i)
+{
+	int		j;
+	char	*buf;
+
+	buf = (char *)malloc(sizeof(char) * (i + 2));
+	if (!buf)
+		return (NULL);
+	j = 0;
+	while (j < i + 1)
+	{
+		buf[j] = line[j];
+		j++;
+	}
+	buf[j] = 0;
 	return (buf);
 }
 
 char	*cut_line(char *line, char *remain)
 {
-	int	i;
+	int		i;
+	int		line_len;
 
+	line = ft_strjoin(remain, line);
 	i = 0;
-	while (line[i] != '\n')
+	line_len = ft_strlen(line);
+	while ((line[i] != '\n') && line[i])
 		i++;
-	remain = *(line + i);
+	if (i + 1 >= line_len)
+		return (line);
+	remain = (line + i + 1);
+	line = edit_line(line, i);
+	return (line);
+}
+
+int	check_newline(char *line, char *remain)
+{
+	int	line_len;
+
+	line_len = ft_strlen(line);
+	if (line[lin_len - 1] == '\n')
+		return (1);
+	else
+	{
+		remain = line;
+		return (0);
+	}
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*remain;
-	char	*line;
+	char		*line;
 
-	if (BUFFER_SIZE =< 0)
+	if (BUFFER_SIZE <= 0)
 		return (NULL);
 	line = read_line(fd);
+	if (!line)
+		return (NULL);
 	line = cut_line(line, remain);
+	if (check_newline(line, remain))
+		return (line);
 }
